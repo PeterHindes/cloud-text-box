ff5e13c6d0ca07c22a05cb2d07aa3517dab63120
Peter Hindes <19994487+PeterHindes@users.noreply.github.com>
2022-06-07 16:55:06 -0600

    cool
    the difff is sent do the server which sends back the current message after update

---
+++ app/src/frontend/src/App.svelte
@@ -0,0 +1,56 @@
+<script>
+	let ws = new WebSocket("ws://localhost:8081");
+	let txtBox = "";
+	let lastTxt;
+	let gotfromServer = "";
+	ws.onmessage = message => {
+		console.log(`Received: ${message.data}`)
+		if (message.data.split("\0")[0] == "update"){ // Key value lamda it
+			txtBox = message.data.replace("update\0","");
+			lastTxt = txtBox;
+			gotfromServer = message.data.replace("update\0","");
+		}
+	};
+	ws.onopen = () => {
+		// ws.send("Hello! I'm client")
+	}
+	function handleUpdate(){
+		console.log("Update");
+		for(let i = 0; i < (txtBox.length > lastTxt.length ? txtBox.length : lastTxt.length); i++){
+			if (txtBox[i] !== lastTxt[i]) {
+				console.log("Diff "+txtBox[i] +" vs "+ lastTxt[i]);
+				ws.send("update\0"+txtBox[i]+"\0"+parseInt(i));
+				break;
+			}
+		}
+		// lastTxt = txtBox;
+		txtBox = lastTxt;
+	}
+</script>
+
+<main>
+	<input bind:value={txtBox} on:input={handleUpdate}>
+	<p>{gotfromServer}</p>
+</main>
+
+<style>
+	/* main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	p {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	} */
+</style>
--- app/src/index.js
+++ app/src/index.js
@@ -6,19 +6,24 @@ const async = require('async')
 
 const APPID = process.env.APPID;
 let connections = [];
+let resumableConnections = [];
 const WebSocketServer = ws.server
 
 
 //create a raw http server (this will help us create the TCP which will then pass to the websocket to do the job)
-const httpserver = http.createServer(function(request, response) {  
-    response.writeHeader(200, {"Content-Type": "text/html"});
-    fs.readFile('./index.html', 'utf8', function (err,data) {
+const public = "./frontend/public"
+const httpserver = http.createServer(function (req, res) {
+    if (req.url == "/") {
+        req.url = "/index.html"
+    }
+    fs.readFile(public + req.url, function (err,data) {
         if (err) {
-            return console.log(err);
+            res.writeHead(404);
+            res.end(JSON.stringify(err));
+            return;
         }
-        // console.log(data);
-        response.write(data);
-        response.end();
+        res.writeHead(200);
+        res.end(data);
     });
 })
 
@@ -33,45 +38,78 @@ httpserver.listen(8081, () => console.log("My server is listening on port 8081")
 //Object to manage connection state
 
 function Handler(con,position) {
+    this.authenticatedAs = 1234; // Test value
+    this.txtBoxContent = "serverside";
+
     this.con = con;
+    this.position = position;
+
+    /*Lifecycle
+    * The client opens the page
+    * The client opens the websocket connection
+    * The user types some new stuff
+    * The client authenticates
+    * The server gets the in memory value from the database and appends the current value to the database
+    * The server pushes the users textbox to the client
+    *   potential features:
+    *       offline editing and send updates on serviceworker
+    *       multiple users edit shared note
+    *       edit history
+    * The user types and it is sent to the server every keystroke (the full message) (update to send the key that was pressed, then return the message the user will see) (data returned will be symbolUTF8 + position)
+    * After the user stops sending updates or a timeout, the textbox is pushed to the database
+    * The user disconnects
+    */
+    this.clientFunctions = {
+        update: async (txtU,pos) => {
+            // console.log(txtU);
+            t = this.txtBoxContent;
+            this.txtBoxContent = t.slice(0,pos)+txtU+t.slice(pos);
+            // console.log(this.txtBoxContent);
+            // console.log(this);
+            this.con.send("update\0"+this.txtBoxContent)
+        },
+        authenticate: async (uname,pass) => {
+            // Your authentication code
+            resumableConnections[this.authenticatedAs] = connections[this.position];
+            connections.splice(this.position,1)
+        },
+        redeemAuth: async (token) => {
+            // lookup token in db
+    
+            // set auth to token uid
+            let resuming = 1234; // TODO REMOVE AND REPLACE WITH DB
+            this.con.send("loginSuc " + this.authenticatedAs);
+    
+            resumableConnections[resuming] = this.con;
+            connections.splice(this.position,1)
+    
+    
+            // if not in db
+            //this.con.send("loginFail ");
+        }    
+    }
+
     this.con.on("close", () => {
         console.log("CLOSED!!!")
         // Remove self from connection list
-        // console.log(connections);
-        // console.log(
-            connections.splice(position,1)
-        // );
-        // console.log(connections);
+        if(this.authenticatedAs != -1){
+            console.log("terminating con");
+            connections.splice(this.position,1)
+        }
     })
     this.con.on("message", message => {
-        console.log(`${APPID} Received message: ${message.utf8Data}`)
-        if (message.utf8Data.split("\0")[0] == "update:"){
-            this.updateTxt(message.utf8Data.replace("update:\0",""));
-        }
+        console.log(`Received message: ${message.utf8Data}`)
+        let slices = message.utf8Data.split("\0");
+        this.clientFunctions[slices.shift()].apply(this,slices);
+        // if (message.utf8Data.split(":\0")[0] == "update"){
+        //     this.update(message.utf8Data.replace("update\0",""));
+        //     this.con.send("update\0"+this.txtBoxContent)
+        // }
     });
-
-    this.authenticatedAs = -1;
-
-    this.txtBoxContent = "";
-
-    this.updateTxt = async function(txtU) {
-        this.txtBoxContent = txtU;
-    }
+    // while (true) {
+    this.con.send("update\0"+this.txtBoxContent)
+    // }
     
-    this.authenticate = async function(uname,pass) {
-        // Your authentication code
-    }
-    this.redeemAuth = async function(token) {
-        // lookup token in db
-
-        // set auth to token uid
-        this.authenticatedAs = 1234; // TODO REMOVE AND REPLACE WITH DB
-
-        this.con.send("loginSuc " + this.authenticatedAs);
-
-        // if not in db
-        //this.con.send("loginFail ");
-    }    
 }
 
 //when a legit websocket request comes listen to it and get the connection .. once you get a connection thats it! 
@@ -80,8 +118,9 @@ websocket.on("request", request=> {
     const con = request.accept(null, request.origin)
     console.log("open");
     
-    con.send(`Connected successfully to server ${APPID}`);
-    
+    // con.send(`Connected successfully to server ${APPID}`);
+    con.send('Opening Greeting')
+
     const hand = new Handler(con, connections.length);
     connections.push(hand);
 })
